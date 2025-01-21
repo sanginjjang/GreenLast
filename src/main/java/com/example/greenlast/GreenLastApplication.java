db2b93966d4b1f408a84b068d245fab27cdc75e1
Lee Dongha <dlehsk43@gmail.com>
2025-01-21 17:07:58 +0900

    test1

---
+++ src/main/java/com/example/greenlast/GreenLastApplication.java
@@ -0,0 +1,13 @@
+package com.example.greenlast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenLastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenLastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/greenlast/GreenLastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greenlast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenLastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
