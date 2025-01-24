7a50c22e5db3ee0833c4bfcaadb532b0556b32f0
Arydong <dlehsk43@gmail.com>
2025-01-24 09:15:42 +0900

    25.01.24.(금) 09:15 관리자 메인 초기안 커밋(차트 내용 수정예정)

---
+++ src/main/java/com/example/greenlast/dto/AgeGroupDTO.java
@@ -0,0 +1,20 @@
+package com.example.greenlast.dto;
+
+import lombok.Data;
+
+/**
+ * packageName    : com.example.greenlast.dto
+ * fileName       : AgeGroupDTO
+ * author         : 이동하
+ * date           : 25. 1. 23.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 25. 1. 23.        이동하       최초 생성
+ */
+@Data
+public class AgeGroupDTO {
+    private String ageGroup;
+    private int count;
+}
