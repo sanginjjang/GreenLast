5cd8257ec25d36ba6c00ad453da69e3c87735dc9
HanSangin <hansam8456@gamil.com>
2025-02-03 11:52:18 +0900

    2025.02.02 11:52 aside 모달 css 침범 수정.
    Signed-off-by: HanSangin <hansam8456@gamil.com>

--- src/main/java/com/example/greenlast/controllers/main/MainController.java
+++ src/main/java/com/example/greenlast/controllers/main/MainController.java
@@ -10,10 +10,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class MainController {
-    @ResponseBody
     @GetMapping("/")
     public String index() {
-        return "성공적으로 연결되었습니다";
+        return "/dongha/home";
     }
     
 }
