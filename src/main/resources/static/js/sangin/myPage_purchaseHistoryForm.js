cab4708f8434ba70096baed4eff0020525c02d5e
HanSangin <hansam8456@gamil.com>
2025-02-07 12:54:37 +0900

    2025.02.07 12:54 1. header, nav 각각 링크 달기.
    Signed-off-by: HanSangin <hansam8456@gamil.com>

--- src/main/resources/static/js/sangin/myPage_purchaseHistoryForm.js
+++ src/main/resources/static/js/sangin/myPage_purchaseHistoryForm.js
@@ -20,15 +20,16 @@ document.addEventListener("DOMContentLoaded", function () {
                         </div>
                         <div class="pur_box_right">
                             <div class="pur_right_money">${history.price}원</div>
-                            <div class="pur_right_delete">
-                                <a href="/api/mypage/purchaseHistory/delete/pno?pno=${history.paymentId}">내역삭제</a>
-                            </div>
-                            <div class="pur_right_refund">환불신청</div>
+                            
+                            <div class="pur_right_refund">환불신청 (아직)</div>
                         </div>
                     </div>
                 `;
             });
         })
+        // <div class="pur_right_delete">
+        //     <a href="/api/mypage/purchaseHistory/delete/pno?pno=${history.paymentId}">내역삭제</a>
+        // </div>
         .catch(error => {
             console.error("결제 내역을 불러오는 중 오류 발생:", error);
         });
