d1cb86abd9d1001e85f81dcf17898611995f74a2
Arydong <dlehsk43@gmail.com>
2025-02-13 12:28:00 +0900

    25.02.13.(목) 12:27 관리자가 강의 승인하는거 진행할거임

---
+++ src/main/java/com/example/greenlast/controllers/api/dongha/PermitClassControllerBack.java
@@ -0,0 +1,25 @@
+package com.example.greenlast.controllers.api.dongha;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : com.example.greenlast.controllers.api.dongha
+ * fileName       : PermitClassControllerBack
+ * author         : 이동하
+ * date           : 25. 2. 13.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 25. 2. 13.        이동하       최초 생성
+ */
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class PermitClassControllerBack {
+
+
+}
